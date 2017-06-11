cb39998f73a048738f78d6973bc044476f4e8045
Roman Chopovenko <romachopovenko@gmail.com>
2017-06-11 20:03:19 +0300

    Initial Commit

---
+++ GraphTestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GraphTestProject
+//
+//  Created by Roman Chopovenko on 6/11/17.
+//  Copyright © 2017 Roman Chopovenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GraphTestProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GraphTestProject
+//
+//  Created by Roman Chopovenko on 6/11/17.
+//  Copyright © 2017 Roman Chopovenko. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
